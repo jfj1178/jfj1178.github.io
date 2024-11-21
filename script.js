2bf38d058ee2991f689df433fe431b8484778dee
jfj1178 <hifiwala1@gmail.com>
2024-11-21 19:31:27 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("messageButton").addEventListener("click", function() {
+  const message = document.getElementById("message");
+  message.textContent = "Hello! Thanks for visiting the website!";
+  message.style.color = "green";
+});
