a230cc8488109339f7c6d88ddf7346505013ad2c
jfj1178 <hifiwala1@gmail.com>
2024-11-24 09:27:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("messageButton").addEventListener("click", function() {
   const message = document.getElementById("message");
-  message.textContent = "Hello! Thanks for visiting the website!";
+  message.textContent = "Hello! Thanks for visiting Arudra's website Scroll down for the form";
   message.style.color = "green";
 });
